9d3a6db64f6cd973d6c417463671008a664a5927
Ritesh Saxena <riteshsax007@gmail.com>
2018-08-16 12:09:12 +0000

    riva: Remove CNE and slim_deamon for now
    Change-Id: I1d69d661dee8eb2a344fe7c3b13353ce211f0fc9

--- riva/riva-vendor.mk
+++ riva/riva-vendor.mk
@@ -1,3 +1,4 @@
+
 # Copyright (C) 2018 The Lineage Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -2125,7 +2126,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/riva/proprietary/vendor/bin/qti:$(TARGET_COPY_OUT_VENDOR)/bin/qti \
     vendor/xiaomi/riva/proprietary/vendor/bin/rmt_storage:$(TARGET_COPY_OUT_VENDOR)/bin/rmt_storage \
     vendor/xiaomi/riva/proprietary/vendor/bin/sensors.qcom:$(TARGET_COPY_OUT_VENDOR)/bin/sensors.qcom \
-    vendor/xiaomi/riva/proprietary/vendor/bin/slim_daemon:$(TARGET_COPY_OUT_VENDOR)/bin/slim_daemon \
     vendor/xiaomi/riva/proprietary/vendor/bin/tftp_server:$(TARGET_COPY_OUT_VENDOR)/bin/tftp_server \
     vendor/xiaomi/riva/proprietary/vendor/bin/thermal-engine:$(TARGET_COPY_OUT_VENDOR)/bin/thermal-engine \
     vendor/xiaomi/riva/proprietary/vendor/bin/time_daemon:$(TARGET_COPY_OUT_VENDOR)/bin/time_daemon \
@@ -2507,7 +2507,6 @@ PRODUCT_PACKAGES += \
     QtiTelephonyService \
     SimSettings \
     uceShimService \
-    CNEService \
     com.qualcomm.location \
     qcrilmsgtunnel \
     colorservice \
